73ef27ce5a09dec256007bedcc69fa7fdc33ccd6
ch <mkmsacc@outlook.com>
2023-07-30 12:37:12 +0700

    Add tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests extends AbstractTestContainersSetup {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void shouldSendAndGetMessage() {
+        var input = "hello";
+        client.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/echo")
+                        .queryParam("input", input)
+                        .build())
+                .exchange()
+                .expectBody(String.class)
+                .value(s -> Assertions.assertThat(s)
+                        .isEqualTo(input));
+
+    }
 
 }
