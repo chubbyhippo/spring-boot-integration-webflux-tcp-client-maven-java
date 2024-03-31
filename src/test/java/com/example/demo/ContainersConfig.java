45439ee9b507ddca6b635865a4b9b9ac7acf16b7
chubbyhippo <mkmsacc@outlook.com>
2024-03-31 11:16:19 +0700

    Add ContainersConfig for test environment
    A new configuration class, ContainersConfig, has been added. This sets up a test environment with the help of 'testcontainers'. The configuration uses a ComposeContainer that utilizes a 'compose.yaml' file from resource directories.

---
+++ src/test/java/com/example/demo/ContainersConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.io.File;
+import java.time.Duration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ContainersConfig {
+
+    @Bean
+    ComposeContainer composeContainer(DynamicPropertyRegistry registry) {
+
+        var compose = new ComposeContainer(new File("src/test/resources/compose.yaml"))
+                .withExposedService("app",
+                        9876,
+                        Wait.forListeningPort()
+                                .withStartupTimeout(Duration.ofMinutes(10)))
+                .withLocalCompose(true);
+
+        registry.add("tcp.server.host", () -> compose.getServiceHost("app", 9876));
+        registry.add("tcp.server.port", () -> compose.getServicePort("app", 9876));
+
+        return compose;
+    }
+}
