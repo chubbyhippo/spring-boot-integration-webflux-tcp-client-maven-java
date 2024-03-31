787dc0aaa894d5d29e39ed7f872431d914a64887
chubbyhippo <mkmsacc@outlook.com>
2024-03-31 11:16:28 +0700

    Integrate ContainersConfig in DemoApplicationTests
    The addition of the ContainersConfig class to the DemoApplicationTests facilitates the setup of a test environment via 'testcontainers'. This approach utilizes a ComposeContainer that reads from a 'compose.yaml' file, thus effectively streamlining the configuration process.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class DemoApplicationTests extends AbstractTestContainersSetup {
+@Import(ContainersConfig.class)
+class DemoApplicationTests {
 
     @Autowired
     private WebTestClient client;
